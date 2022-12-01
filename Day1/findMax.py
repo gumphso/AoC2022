ad5f386fb3a72a65638953b17afaef3774f9bdac
gumphso <60444720+gumphso@users.noreply.github.com>
2022-12-01 03:05:59 -0500

    Day One Part 2

--- Day1/findMax.py
+++ Day1/findMax.py
@@ -1,18 +1,23 @@
 
 elfcount, elfTrack, calorieCount, calorieMax = 0,0,0,0
-
+topThreearray = [0,0,0]
 with open('Day1\input.txt') as f:
-    for line in f:
-        if line != "\n":
+    while True:
+        line = f.readline()
+        if line != "\n" and len(line) != 0:
             calorieCount += int(line)
         else:
             elfcount += 1
             if calorieCount > calorieMax:
                 elfTrack = elfcount
-                calorieMax = calorieCount
+            if calorieCount > min(topThreearray):
+                topThreearray[topThreearray.index(min(topThreearray))] = calorieCount
+                calorieMax = topThreearray[topThreearray.index(max(topThreearray))]
             calorieCount = 0
+            if len(line) == 0:
+                break
 
-print("Elf #"+str(elfTrack)+" with "+str(calorieMax)+" total calories")
+print("Top Elf is #"+str(elfTrack)+" with "+str(calorieMax)+" total calories, sum of top three is "+str(sum(topThreearray)))
 
 
 
